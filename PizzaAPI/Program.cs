d83a274e4270a8632d830b35de2cfaf64c5ec792
yjohnson67 <joh23004@byui.edu>
2026-04-23 16:15:11 -0500

    Completed Unit 01 assignment

--- PizzaAPI/Program.cs
+++ PizzaAPI/Program.cs
@@ -1,41 +1,41 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+List<Pizza> pizzas = new()
 {
-    app.MapOpenApi();
-}
+    new Pizza(1, "Cheese", false),
+    new Pizza(2, "Pepperoni", false),
+    new Pizza(3, "Veggie", true),
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    // Added extra record for assignment
+    new Pizza(4, "Hawaiian", false)
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/", () => "Pizza API Running!");
+
+app.MapGet("/pizzas", () => pizzas);
+
+app.MapGet("/pizzas/{id}", (int id) =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    var pizza = pizzas.FirstOrDefault(p => p.Id == id);
+    return pizza is not null ? Results.Ok(pizza) : Results.NotFound();
+});
+
+app.MapPost("/pizzas", (Pizza pizza) =>
+{
+    pizzas.Add(pizza);
+    return Results.Created($"/pizzas/{pizza.Id}", pizza);
+});
+
+app.MapDelete("/pizzas/{id}", (int id) =>
+{
+    var pizza = pizzas.FirstOrDefault(p => p.Id == id);
+    if (pizza is null) return Results.NotFound();
+
+    pizzas.Remove(pizza);
+    return Results.NoContent();
+});
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+record Pizza(int Id, string Name, bool IsGlutenFree);
